420f4f3f09b76e3114e3ba112c57363ef19464f5
ashwinibm <ashwini@baysidetech.in>
2018-04-13 16:03:46 +0530

    Added save button for each attachment

--- src/views/forms/FileUpload.js
+++ src/views/forms/FileUpload.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {FormControl} from './FormControl';
-import { Label, Input, Row, Col} from 'reactstrap';
+import { Label, Input, Row, Col, Button} from 'reactstrap';
 
 
 /**
@@ -12,49 +12,55 @@ import { Label, Input, Row, Col} from 'reactstrap';
  */
 class FileUpload extends React.Component {
 
+    handleSaveFile() {
+        console.log("Save attachment");
+    }
+
     render() {
         const {fileValue, title, fileType, fileName} = this.props ;
         console.log(" PROPS = fileValue, title, fileType, fileName ", fileValue, title, fileType, fileName);
         const {onChangeFile, onChangeFileTitle, commonkey} = this.props ; 
         return (
-             <Row className="mb-4">
-                <Col xs="12">
-                    <Row>
-                      <Col xs="12">
-                        <input type="file" className="form-control-file" forkey={commonkey} id="file" name="file" onChange={onChangeFile} />
-                       
-                      </Col>  
-                    </Row>
-                    <Row>
-                      <Col xs="6">
-                        <FormControl>
-                            <Label htmlFor="docAttType">Upload File Type</Label>
-                            <input type="text" name="docAttType" 
-                                forkey={commonkey}
-                                className="form-control"
-                                value={ fileType } 
-                                id="doctype"
-                                readOnly="true"
-                                placeholder="Upload File Type" required/>
-                             
-                        </FormControl>
-                      </Col>
-                      <Col xs="6">
-                        <FormControl>
-                            <Label htmlFor="docAttTitle">File Title</Label>
-                            <Input type="text" name="docAttTitle" 
-                                forkey={commonkey}
-                                className="form-control"
-                                onChange={ onChangeFileTitle }
-                                value={ title }  
-                                id="doctitle"
-                                placeholder="File Title" required/>
-                           
-                        </FormControl>
-                      </Col>
-                    </Row>
-                </Col> 
-            </Row>
+            <div>
+                <Row className="mb-4">
+                    <Col xs="12">
+                        <Row>
+                        <Col xs="12">
+                            <input type="file" className="form-control-file" forkey={commonkey} id="file" name="file" onChange={onChangeFile} />
+                        </Col>
+                        </Row>
+                        <Row>
+                        <Col xs="6">
+                            <FormControl>
+                                <Label htmlFor="docAttType">Upload File Type</Label>
+                                <input type="text" name="docAttType"
+                                    forkey={commonkey}
+                                    className="form-control"
+                                    value={ fileType }
+                                    id="doctype"
+                                    readOnly="true"
+                                    placeholder="Upload File Type" required/>
+                            </FormControl>
+                        </Col>
+                        <Col xs="6">
+                            <FormControl>
+                                <Label htmlFor="docAttTitle">File Title</Label>
+                                <Input type="text" name="docAttTitle"
+                                    forkey={commonkey}
+                                    className="form-control"
+                                    onChange={ onChangeFileTitle }
+                                    value={ title }
+                                    id="doctitle"
+                                    placeholder="File Title" required/>
+                            </FormControl>
+                        </Col>
+                        </Row>
+                    </Col>
+                </Row>
+                <Button size="sm" color="primary" className="float-right" onClick={this.handleSaveFile.bind(this)}>
+                    <i className="fa fa-dot-circle-o"></i> Save
+                </Button>
+            </div>
         );
     }
 };
