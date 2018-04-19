428f5a1000aa618e9dfebf411800c820358f0e35
kohsah <ashok@hariharan.org.in>
2018-04-20 00:11:24 +0530

    Fixes to UI

---
+++ src/views/forms/DocumentForm.stateManager.js
@@ -0,0 +1,81 @@
+import { STATE_ACTION_RESET, STATE_ACTION_IS_SUBMITTING, STATE_ACTION_SET_DOCUMENT_LOAD_ERROR, STATE_ACTION_IS_NOT_SUBMITTING, STATE_ACTION_SET_FIELD_VALUE, STATE_ACTION_LOADED_DATA, STATE_ACTION_SET_FIELD_ERROR } from './DocumentForm.constants';
+import {identityInitialState} from './DocumentForm.formConfig';
+/** STATE HANDLERS  */
+
+export const stateAction = (state, action) => {
+    switch (action.type) {
+      case STATE_ACTION_RESET:
+        return Object.assign(
+          {}, 
+          state, 
+          {pkg: {pkgIdentity: identityInitialState()}}
+        );
+      case STATE_ACTION_IS_SUBMITTING:
+        return Object.assign(
+          {}, 
+          state, 
+          {isSubmitting: true}
+        );
+      case STATE_ACTION_IS_NOT_SUBMITTING:
+        return Object.assign(
+          {}, 
+          state, 
+          {isSubmitting: false}
+        );
+      case STATE_ACTION_LOADED_DATA:
+        // action.params = {isSubmitting: true / false,   pkg: {pkgIdentity: aknDoc}}
+        return Object.assign(
+          {}, 
+          state, 
+          {isSubmitting: false, pkg: {pkgIdentity: action.params.aknDoc}}
+        );
+      case STATE_ACTION_SET_FIELD_VALUE:
+        return Object.assign(
+          {},
+          state,
+          {
+            pkg: {
+                pkgIdentity: {
+                    ...state.pkg.pkgIdentity, 
+                    [action.params.fieldName]: {
+                        ...state.pkg.pkgIdentity[action.params.fieldName], 
+                        value: action.params.fieldValue,
+                        error: null
+                    }
+                }
+            }
+          }
+        );
+      case STATE_ACTION_SET_FIELD_ERROR:
+        return Object.assign(
+          {},
+          state,
+          {
+            pkg:{
+                pkgIdentity: {
+                    ...state.pkg.pkgIdentity, 
+                      [action.params.fieldName]: {
+                        ...state.pkg.pkgIdentity[action.params.fieldName], 
+                        value: action.params.err.value === null ? '': action.params.err.value,
+                        error: action.params.err.message
+                    }
+                }
+              }
+          }
+        );
+      case STATE_ACTION_SET_DOCUMENT_LOAD_ERROR:
+        return Object.assign(
+          {},
+          state,
+          {
+            documentLoadError: true
+          }
+        );
+      default:
+        return state;
+    }
+};
+
+export const applyActionToState = (THIS, action) => {
+  THIS.setState(stateAction(THIS.state, action));
+};
--- src/views/forms/DocumentFormActions.js
+++ src/views/forms/DocumentFormActions.js
@@ -7,20 +7,30 @@ import {linkDocumentAdd} from '../../components/utils/QuickRoutes';
 
 export const DocumentFormActions = ({lang}) => {
     return(
-        <Card className="bg-white text-right mt-1 mb-1">
-        <CardBody className="pt-0 pb-0 pl-0 pr-0">
+        <Card className={`bg-white text-right mt-1 mb-1`}>
+           <CardBody className={`pt-0 pb-0 pl-0 pr-0`}>
             <ButtonGroup>
-                <Button type="button" className={ `btn btn-link` } >
-                    <NavLink to={ linkDocumentAdd(lang) }>
+                <button type="button" class="btn btn-link btn btn-secondary"><a aria-current="false" href="/_lang/en/document/add"><i class="fa fa-plus"></i> Add Document</a></button><button type="button" class="btn btn-link btn btn-secondary">Select All</button>
+            </ButtonGroup>
+            </CardBody>
+        </Card>        
+
+        /*         <div class="bg-white text-right mt-1 mb-1 card"><div class="pt-0 pb-0 pl-0 pr-0 card-body"><div role="group" class="btn-group"><button type="button" class="btn btn-link btn btn-secondary"><a aria-current="false" href="/_lang/en/document/add"><i class="fa fa-plus"></i> Add Document</a></button><button type="button" class="btn btn-link btn btn-secondary">Select All</button></div></div></div>         */
+
+/*         <Card className={`bg-white text-right mt-1 mb-1`}>
+            <CardBody className={`pt-0 pb-0 pl-0 pr-0`}>
+                <ButtonGroup>
+                    <Button type="button" className={ `btn btn-link` } >
+                        <NavLink to={ linkDocumentAdd(lang) }>
                             <i className="fa fa-plus"></i> Add Document
-                    </NavLink>
-                </Button>
-                <Button type="button" className={ `btn btn-link` } onClick={ () => console.log(" Hello World!")}>
-                    Select All
-                </Button>
-            </ButtonGroup>                
-        </CardBody>
-        </Card>
+                        </NavLink>
+                    </Button>
+                    <Button type="button" className={ `btn btn-link` } onClick={ () => console.log(" Hello World!")}>
+                        Select All
+                    </Button>
+                </ButtonGroup>                
+            </CardBody>
+        </Card> */
     );
 };
 
