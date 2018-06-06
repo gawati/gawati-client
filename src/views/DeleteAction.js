c266704881ed67f3e8029095611bbaea8843f269
kohsah <ashok@hariharan.org.in>
2018-06-06 11:44:34 +0530

    Adding message for delete confirmation

--- src/views/DeleteAction.js
+++ src/views/DeleteAction.js
@@ -21,11 +21,14 @@ class DeleteAction extends React.Component {
     const {action,deleteDoc,linkIri} = this.props;
     return (<span><Button className="btn btn-info" role="button" onClick= {this.toggleModal.bind(this)} >{T(action.label)}</Button>
             <ConfirmModal show={this.state.isOpen}
-                          onClose={this.toggleModal.bind(this)}
-                          title={T("Are you sure you want to delete the selected document?")}
-                          onOK={() => deleteDoc(linkIri)}
-                          onOKLabel={T("Ok")} 
-                          onCloseLabel={T("Cancel")} /></span>
+                onClose={this.toggleModal.bind(this)}
+                title={T("Are you sure you want to delete the selected document?")}
+                onOK={() => deleteDoc(linkIri)}
+                onOKLabel={T("Ok")} 
+                onCloseLabel={T("Cancel")} >
+                The Document will be deleted !
+            </ConfirmModal>                  
+            </span>
     )        
   }
 }
